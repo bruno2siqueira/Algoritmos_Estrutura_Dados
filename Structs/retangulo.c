13f9fe603ba01fc5d12de55758e8e63ed1bda9eb
Bruno Siqueira da Silva <bruno2siqueira@users.noreply.github.com>
2026-08-24 20:16:52 -0300

    Update retangulo.c

--- Structs/retangulo.c
+++ Structs/retangulo.c
@@ -24,5 +24,10 @@ int main()
     printf("Retangulo 1: Ponto 2: (%d, %d)\n", r1.p2.x, r1.p2.y);
     printf("Retangulo 1: %d bytes", sizeof(r1.p1.x) + sizeof(r1.p1.y) + sizeof(r1.p2.x) + sizeof(r1.p2.y));
 
+    struct Retangulo r2 = {{10, 20}, {30, 40}}; // Inicialização direta
+    printf("\nRetangulo 2: Ponto 1: (%d, %d)\n", r2.p1.x, r2.p1.y);
+    printf("Retangulo 2: Ponto 2: (%d, %d)\n", r2.p2.x, r2.p2.y);
+    printf("Retangulo 2: %d bytes", sizeof(r2.p1.x) + sizeof(r2.p1.y) + sizeof(r2.p2.x) + sizeof(r2.p2.y));
+
     return 0;
 }
